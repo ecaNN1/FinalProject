76bad9bf4c7aed89cbdac68ae0c975464f27be83
ecaNN1 <129431360+ecaNN1@users.noreply.github.com>
2023-10-03 13:13:16 +0300

    Add files via upload

---
+++ Genre.cs
@@ -0,0 +1,24 @@
+﻿using FinalProject.Core.Enums;
+using FinalProject.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.Core.Entities
+{
+    public class Genre : IBaseEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public DateTime? DeleteDate { get; set; }
+        public Status Status { get; set; }
+
+        //Navigation Prop
+        public virtual List<Post> Posts { get; set; }
+    }
+}
---
+++ Post.cs
@@ -0,0 +1,31 @@
+﻿using FinalProject.Core.Enums;
+using FinalProject.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.Core.Entities
+{
+    public class Post : IBaseEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public DateTime? DeleteDate { get; set; }
+        public Status Status { get; set; }
+
+        //Navigation Prop
+        public int GenreId { get; set; }
+        public virtual Genre Genre { get; set; }
+
+        public string AppUserId { get; set; }
+        public virtual AppUser AppUser { get; set; }
+
+        public virtual List<Comment> Comments { get; set; }
+        public virtual List<Like> Likes { get; set; }
+    }
+}
